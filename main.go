57b605ef641ee6ba2fc9153fb8a8d76c5d500ac4
Tri Ardini <tr.ardn@gmail.com>
2019-06-18 11:22:45 +0700

    complete code for uploading json to s3

--- main.go
+++ main.go
@@ -18,23 +18,28 @@ type Request struct {
 func HandleRequest(req Request) (string, error) {
 	hiddenValue, _ := req.Data["form_response"].(map[string]interface{})["hidden"].(map[string]interface{})
 
+	var fileName string
+	var path string
 	for i, identifier := range hiddenValue {
 		// iterate hidden value to determine path-to-save and file name of json file
+		if i == "id" {
+			fileName = fmt.Sprintf("%s.json", identifier)
+		}
+
+		if i == "model" {
+			path = fmt.Sprintf("%s/%s", path, identifier)
+		}
 	}
+
+	bucket := fmt.Sprintf("internal-response-staging/%s", path)
 
 	data, err := json.Marshal(req.Data)
 	if err != nil {
 		return "", err
 	}
 
-	/*
-		Block code for saving data to json file
-		still looking for best method to save the response to json file
-	*/
-
-	// http://bucket.s3.aws-region.amazonaws.com
-	bucket := "http://s3.ap-southeast-1.amazonaws.com/internal-response-staging/"
-	filename := "test-aja.json"
+	// bucket := "assets.kitabisa.xyz"
+	// filename := "test-aja.json"
 
 	// create a reader from data data in memory
 	reader := strings.NewReader(string(data))
@@ -46,16 +51,14 @@ func HandleRequest(req Request) (string, error) {
 
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
-		Key:    aws.String(filename),
-		// here you pass your reader
-		// the aws sdk will manage all the memory and file reading for you
-		Body: reader,
+		Key:    aws.String(fileName),
+		Body:   reader,
 	})
 	if err != nil {
-		fmt.Printf("Unable to upload %q to %q, %v", filename, bucket, err)
+		fmt.Printf("Unable to upload %q to %q, %v", fileName, bucket, err)
 	}
 
-	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+	fmt.Printf("Successfully uploaded %q to %q\n", fileName, bucket)
 
 	return "Test", nil
 }
